4bba1c425d939d848974079cba84ec009449ad74
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2022-10-23 14:11:42 +0200

    Some bugfix :  - edit cmdline  - edit image export

---
+++ stage-k3s/00-sys-tweaks/00-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
--- stage-k3s/00-sys-tweaks/01-run-chroot.sh
+++ stage-k3s/00-sys-tweaks/01-run-chroot.sh
@@ -1,6 +1,3 @@
 # Enabling legacy iptables on Raspberry Pi OS
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-
-# Enabling cgroups for Raspberry Pi OS
-sed -i -e 'cgroup_memory=1 cgroup_enable=memory' /boot/cmdline.txt 
